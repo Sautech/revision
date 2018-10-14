f7d67b3b120cecd0026efb1845200ed859c82da9
Saurabh <saurabh.anil12@gmail.com>
2018-10-14 15:45:55 +0530

    allowed

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Equipments
+from app.models import Equipments, Members, Users
 
 admin.site.register(Equipments)
 
+admin.site.register(Members)
+
+admin.site.register(Users)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-import RPi.GPIO as GPIO
 
 # Create your views here.
 from django.urls import reverse_lazy
